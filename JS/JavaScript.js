f9fb75a32b2a1964c8f7d0aa5867a9d39686a9fb
Javad Khajavi <javad_khajavi@yahoo.com>
2022-09-30 13:28:06 +0200

    replaced old JS with new version

--- JS/JavaScript.js
+++ JS/JavaScript.js
@@ -2,25 +2,23 @@
 
 
 /* Building the dynamic navigation menu */
-
-
 const menu = document.getElementById('navbarList'); // defining variable for the menu
 const sections = document.querySelectorAll("section"); // Storing all the section elements in the sections variable
 
 
 function navbarBuilder() { // defining a function to create the navbar menu
-	
-	let navMenu = ''; // defines an initially empty variable for the menu
-	
-	sections.forEach(section => {  // for each section runs the function to build the list of the menubar
-		
-		const sectionID = section.id;
-		
-		navMenu += `<li><a href="#${sectionID}">${sectionID}</a></li>` // adds a list elemnt for each section to the menubar
-		
-	});
-	
-	menu.innerHTML = navMenu; // adds the list to the menu
+
+    let navMenu = ''; // defines an initially empty variable for the menu
+
+    sections.forEach(section => { // for each section runs the function to build the list of the menubar
+
+        const sectionID = section.id;
+
+        navMenu += `<li><a onclick="highlightItem(this)" href="#${sectionID}">${sectionID}</a></li>` // adds a list element for each section to the menubar
+
+    });
+
+    menu.innerHTML = navMenu; // adds the list to the menu
 
 }
 
@@ -30,24 +28,38 @@ navbarBuilder();
 /* End of building the dynamic navigation menu */
 
 
+/* Add class HighLight to the selected menu item */
+
+function highlightItem(selectedEle) {
+
+    const allElem = document.querySelectorAll("#navbarList a");
+
+    for (ele of allElem) { // removes highLight class from all elements
+        ele.classList.remove("highLight");
+    }
+    selectedEle.classList.add("highLight"); // adds highLight class to the selected element on the navbar
+}
+
+/* End of class HighLight to the selected menu item */
+
 /* Add class 'active' to section when near top of viewport */
 
 let windowHeight = window.innerHeight || document.documentElement.clientHeight; // defining a variable to contain the hright of the view
 
 function sectionInView(section) {
-	let sectionPosition = section.getBoundingClientRect(); // calculating the bottom position of the section
-	return (sectionPosition.top <= (windowHeight/2) && sectionPosition.bottom >= (windowHeight/2)); //returning true if the section is in view
+    let sectionPosition = section.getBoundingClientRect(); // calculating the bottom position of the section
+    return (sectionPosition.top <= (windowHeight / 2) && sectionPosition.bottom >= (windowHeight / 2)); //returning true if the section is in view
 }
 
 function toggleActiveClass() { // toggle the active class based on the section that is in the view
-	for (section of sections) {
-		
-		if (sectionInView(section)) {
-			section.classList.add('your-active-class');
-		} else {
-			section.classList.remove('your-active-class');
-		}
-	}
+    for (section of sections) {
+
+        if (sectionInView(section)) {
+            section.classList.add('your-active-class');
+        } else {
+            section.classList.remove('your-active-class');
+        }
+    }
 }
 
 window.addEventListener('scroll', toggleActiveClass); // running the function if the user scrolls
@@ -61,3 +73,19 @@ window.addEventListener('scroll', toggleActiveClass); // running the function if
 
 /* Scroll to anchor ID using scrollTO event */
 
+const links = document.querySelectorAll("#navbarList a"); // defining the variable for the menu links 
+
+for (const link of links) { // loop for each menu link, listens to click, and runs clickMenuItem function
+    link.addEventListener("click", clickMenuItem);
+}
+
+function clickMenuItem(i) { // function to prevent deafault, get the href attribute and smoothly scroll to  the top of the section
+    i.preventDefault();
+    const href = this.getAttribute("href");
+    const offsetTop = document.querySelector(href).offsetTop;
+
+    scroll({
+        top: offsetTop,
+        behavior: "smooth"
+    });
+}
